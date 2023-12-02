1cb0e75437ceb68deb9590f3bc8556b260629420
valli0x <youarethesame1821@gmail.com>
2023-12-03 00:48:58 +0300

    gitignore data and json model refactor

--- model/apod.go
+++ model/apod.go
@@ -7,13 +7,13 @@ import (
 )
 
 type APOD struct {
-	gorm.Model
+	gorm.Model     `json:"-"`
 	Copyright      string    `json:"copyright"       mapstructure:"copyright"	gorm:"primaryKey"`
-	Date           time.Time `json:"date"		      mapstructure:"date"		gorm:"index"`
+	Date           time.Time `json:"date"		     mapstructure:"date"		gorm:"index"`
 	Explanation    string    `json:"explanation"     mapstructure:"explanation"`
 	MediaType      string    `json:"media_type"      mapstructure:"media_type"`
 	ServiceVersion string    `json:"service_version" mapstructure:"service_version"`
-	Title          string    `json:"title" 		  mapstructure:"title"`
-	URL            string    `json:"url" 			  mapstructure:"url"`
-	IDData         string    `json:"id_data" 		  mapstructure:"id_data"`
+	Title          string    `json:"title" 		     mapstructure:"title"`
+	URL            string    `json:"url" 			 mapstructure:"url"`
+	IDData         string    `json:"id_data" 		 mapstructure:"id_data"`
 }
